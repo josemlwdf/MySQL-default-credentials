06885546213795a1f1c6a5d00a90623d771fb0b3
josewdf <60181010+josemlwdf@users.noreply.github.com>
2025-02-09 21:39:34 +0100

    Update MySQL_default_creds.py
    Ported to python 3 and changed mysql library to an official one "mysql-connector-python"

--- MySQL_default_creds.py
+++ MySQL_default_creds.py
@@ -1,65 +1,48 @@
+import mysql.connector
 import sys
-import subprocess
-import MySQLdb
 import threading
-import time
 
 def def_cred():
-   """
+    """
+    A function to get default credentials for db access.
+    """
+    with open("default_db_credentials1.txt", "r") as fp:
+        def_db_val = fp.read()
+    return def_db_val.splitlines()
 
-   A function to get default credentials for db access.
-   
-   """
-   fp = open("default_db_credentials1.txt", "r")
-   def_db_val = fp.read()
-   fp.close()
-   return def_db_val.splitlines()
-   
 def connect_mysql_ip_port(ip, port, user="", passwd=""):
-  """
+    """
+    A function to connect ip for that port.
+    """
+    try:
+        mysql.connector.connect(host=ip,
+                                user=user,
+                                password=passwd,
+                                port=port,
+                                connection_timeout=10)
+        print(f"[+] Credentials Found: {user}:{passwd}")
+    except:
+        pass
 
-  A function to connect ip for that port.
-
-  """
-  try:
-      MySQLdb.connect(host=ip,
-                      user=user,
-                      passwd=passwd,
-                      port=port,
-                      connect_timeout=10)
-      print (ip, user, passwd)
-  except:
-      pass
-
-def connect_db(ip, port, db):
-   """
-
-   Function to connect MySQL db with default cred
-
-   """
-   for row in def_cred():
-       user, passwd = row.split(':')
-       try:
-           t1 = threading.Thread \
-                      (target=db,
-                       args=(ip, port, \
-                             user, \
-                             passwd))
-           t1.start()
-       except:
-           print "Failed %s :%s" % (ip, port)
+def connect_db(ip, port, _connect_mysql_ip_port):
+    """
+    Function to connect MySQL db with default cred
+    """
+    for row in def_cred():
+        user, passwd = row.split(':')
+        try:
+            t1 = threading.Thread(target=connect_mysql_ip_port, args=(ip, port, user, passwd))
+            t1.start()
+        except:
+            pass
 		   
 def main(ip, port):
     """
-
     Start of Script.
-    
     """
-    success = []
-    if port == 3306:
-        connect_db(ip, port, connect_mysql_ip_port)
+    connect_db(ip, port, connect_mysql_ip_port)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-      sys.exit("Usage: python MySQL_default_creds.py ip port")
+      sys.exit("Usage: python3 MySQL_default_creds.py ip port")
     main(sys.argv[1], int(sys.argv[2]))
